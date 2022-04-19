7ef8ee06153121b591d903021d4ec3bfa678bd6d
Roman <roman.zuchowski@gmail.com>
2022-04-19 16:58:51 +0200

    💡Changed the font size

--- components/header/navigationSection.js
+++ components/header/navigationSection.js
@@ -4,26 +4,26 @@ import { useRouter } from 'next/router';
 function NavigationSection() {
     const router = useRouter();
     return (
-        <>
+        <div className='text-sm'>
             <Link href='/'> 
-                <a className={router.pathname == "/" ? "font-bold" : ""}>Startseite</a>
+                <a className={router.pathname == "/" ? "font-bold px-2" : "px-2"}>Startseite</a>
             </Link>
             <Link href='/serien'> 
-                <a className={router.pathname == "/serien" ? "font-bold" : ""}>Serien</a>
+                <a className={router.pathname == "/serien" ? "font-bold px-2" : "px-2"}>Serien</a>
             </Link>
             <Link href='/filme'> 
-                <a className={router.pathname == "/filme" ? "font-bold" : ""}>Filme</a>
+                <a className={router.pathname == "/filme" ? "font-bold px-2" : "px-2"}>Filme</a>
             </Link>
             <Link href='/neu'> 
-                <a className={router.pathname == "/neu" ? "font-bold" : ""}>Neu und beliebt</a>
+                <a className={router.pathname == "/neu" ? "font-bold px-2" : "px-2"}>Neu und beliebt</a>
             </Link>
             <Link href='/meins'> 
-                <a className={router.pathname == "/meins" ? "font-bold" : ""}>Meine Liste</a>
+                <a className={router.pathname == "/meins" ? "font-bold px-2" : "px-2"}>Meine Liste</a>
             </Link>
             <Link href='/audio'> 
-                <a className={router.pathname == "/audio" ? "font-bold" : ""}>Audio und Untertitel</a>
+                <a className={router.pathname == "/audio" ? "font-bold px-2" : "px-2"}>Audio und Untertitel</a>
             </Link>
-        </>
+        </div>
     )
 }
 

5fbf98656f037033469431d06ea261bfbd5a5754
Roman <roman.zuchowski@gmail.com>
2022-04-19 20:42:29 +0200

    💡Refactored the nav code

--- components/header/navigationSection.js
+++ components/header/navigationSection.js
@@ -1,28 +1,20 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { sections } from '../../public/assets/sections';
 
 function NavigationSection() {
     const router = useRouter();
     return (
         <div className='text-sm'>
-            <Link href='/'> 
-                <a className={router.pathname == "/" ? "font-bold px-2" : "px-2"}>Startseite</a>
-            </Link>
-            <Link href='/serien'> 
-                <a className={router.pathname == "/serien" ? "font-bold px-2" : "px-2"}>Serien</a>
-            </Link>
-            <Link href='/filme'> 
-                <a className={router.pathname == "/filme" ? "font-bold px-2" : "px-2"}>Filme</a>
-            </Link>
-            <Link href='/neu'> 
-                <a className={router.pathname == "/neu" ? "font-bold px-2" : "px-2"}>Neu und beliebt</a>
-            </Link>
-            <Link href='/meins'> 
-                <a className={router.pathname == "/meins" ? "font-bold px-2" : "px-2"}>Meine Liste</a>
-            </Link>
-            <Link href='/audio'> 
-                <a className={router.pathname == "/audio" ? "font-bold px-2" : "px-2"}>Audio und Untertitel</a>
-            </Link>
+            {
+                Object.entries(sections).map(([key, {name, endpoint}]) => {
+                    return (
+                        <Link key={key} href={endpoint}>
+                            <a className={router.pathname == `${endpoint}` ? 'font-bold px-2' : 'px-2'}>{name}</a>
+                        </Link>
+                    )
+                })
+            }
         </div>
     )
 }

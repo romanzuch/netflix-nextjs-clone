93ba20b2806ce9cb605822484a344e8b713de04e
Roman <roman.zuchowski@gmail.com>
2022-04-20 21:20:29 +0200

    💡Introduced section headings, and some new styling

--- pages/movies.js
+++ pages/movies.js
@@ -1,20 +1,37 @@
-import { movieData } from "../public/assets/data"
-import MediaEntry from "../components/mediaEntry"
+import movieJSON from '../public/assets/movies.json';
+import MediaEntry from "../components/mediaEntry";
 
 export default function Filme() {
+
+  const movieData = getMovieData();
 
   return (
     <div className='m-5'>
         <h1 className="text-6xl">Movies</h1>
-        <div className='flex flex-row p-8'>
-          {movieData.map((data, key) => {
+        <div className='flex flex-col p-8'>
+          {Object.keys(movieData).map((data, key) => {
             return (
-              <div key={key}>
-                <MediaEntry data={data} />
+              <div className='flex flex-col' key={key}>
+                <h1 className='text-4xl mb-8'>{data}</h1>
+                <div className='flex flex-row mb-8'>
+                {
+                  movieData[data].map((data, key) => {
+                    return (
+                      <MediaEntry key={key} data={data} />
+                    ) 
+                  })
+                }
+                </div>
               </div>
             )
           })}
         </div>
     </div>
   )
+}
+
+const getMovieData = () => {
+  let string = JSON.stringify(movieJSON);
+  let data = JSON.parse(string);
+  return data;
 }

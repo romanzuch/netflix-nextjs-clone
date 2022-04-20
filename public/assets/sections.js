d857aa243ee379668969dac688858c4117e65a4d
Roman <roman.zuchowski@gmail.com>
2022-04-20 18:07:00 +0200

    💡Initial commit assets folder

---
+++ public/assets/sections.js
@@ -0,0 +1,8 @@
+export const sections = [
+    {name: 'Start', endpoint: '/'},
+    {name: 'Series', endpoint: '/series'},
+    {name: 'Movies', endpoint: '/movies'},
+    {name: 'New and popular', endpoint: '/new'},
+    {name: 'My list', endpoint: '/list'},
+    {name: 'Audio and subtitles', endpoint: '/audio'}
+]

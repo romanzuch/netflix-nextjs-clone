284f0207db0a13ffc14805588aac5bef64aef885
Roman <roman.zuchowski@gmail.com>
2022-04-24 19:06:51 +0200

    💡Added new image domains and env variables

--- next.config.js
+++ next.config.js
@@ -3,9 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
       domains: [
-          'm.media-amazon.com'
+          'm.media-amazon.com',
+          'image.tmdb.org',
       ]
-  }
+  },
+  env: {
+    API_KEY: process.env.API_KEY,
+  },
 }
 
 module.exports = nextConfig

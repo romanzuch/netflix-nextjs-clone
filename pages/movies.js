155fda8078f772854f4731f59fd5d4a727d5d480
Roman <roman.zuchowski@gmail.com>
2022-04-24 19:09:43 +0200

    💡Removed a lot of unused code due to new API

--- pages/movies.js
+++ pages/movies.js
@@ -1,37 +1,22 @@
-import movieJSON from '../public/assets/movies.json';
-import MediaEntry from "../components/mediaEntry";
+import requests from '../utils/requests';
+import List from '../components/list';
 
 export default function Filme() {
 
-  const movieData = getMovieData();
-
   return (
     <div className='m-5'>
-        <h1 className="text-6xl">Movies</h1>
-        <div className='flex flex-col p-8'>
-          {Object.keys(movieData).map((data, key) => {
-            return (
-              <div className='flex flex-col' key={key}>
-                <h1 className='text-4xl mb-8'>{data}</h1>
-                <container className='flex flex-row h-48 overflow-x-auto no-scrollbar overflow-y-auto p-4'>
-                  {
-                    movieData[data].map((data, key) => {
-                      return (
-                        <MediaEntry key={key} data={data} />
-                      ) 
-                    })
-                  }
-                </container>
-              </div>
-            )
-          })}
+        <h1 className="text-4xl">Movies</h1>
+        <div className='flex flex-col mt-8'>
+          {
+            Object.entries(requests).map(([key, {title, url, section}]) => (
+              <List 
+                key={key} 
+                title={title}
+                endpoint={url}
+              />
+            ))
+          }
         </div>
     </div>
   )
-}
-
-const getMovieData = () => {
-  let string = JSON.stringify(movieJSON);
-  let data = JSON.parse(string);
-  return data;
 }

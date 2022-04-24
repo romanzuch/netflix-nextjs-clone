abff1af1528c99ebdb578501bd733c0d24459600
Roman <roman.zuchowski@gmail.com>
2022-04-24 19:38:00 +0200

    💡Added additional genres

--- utils/requests.js
+++ utils/requests.js
@@ -26,8 +26,38 @@ export default {
         url: `/discover/movie?api_key=${constants.API_KEY}&with_genres=878`,
         section: '878',
     },
+    adventure: {
+        title: 'Adventure',
+        url: `/discover/movie?api_key=${constants.API_KEY}&with_genres=12`,
+        section: '12',
+    },
+    crime: {
+        title: 'Crime',
+        url: `/discover/movie?api_key=${constants.API_KEY}&with_genres=80`,
+        section: '80',
+    },
+    documentary: {
+        title: 'Documentary',
+        url: `/discover/movie?api_key=${constants.API_KEY}&with_genres=99`,
+        section: '99',
+    },
+    fantasy: {
+        title: 'Fantasy',
+        url: `/discover/movie?api_key=${constants.API_KEY}&with_genres=14`,
+        section: '14',
+    },
+    mystery: {
+        title: 'Mystery',
+        url: `/discover/movie?api_key=${constants.API_KEY}&with_genres=9648`,
+        section: '9648',
+    },
+    thriller: {
+        title: 'Thriller',
+        url: `/discover/movie?api_key=${constants.API_KEY}&with_genres=53`,
+        section: '53',
+    },
 };
 
 export const genres = [
-    'popular', 'action', 'comedy', 'horror', 'scifi'
+    'popular', 'action', 'comedy', 'horror', 'scifi', 'adventure', 'crime', 'documentary', 'fantasy', 'mystery', 'thriller'
 ]

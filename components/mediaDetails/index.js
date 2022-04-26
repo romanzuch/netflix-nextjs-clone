965fe06f75816561cfc0827c43983eb978d15c05
Roman <roman.zuchowski@gmail.com>
2022-04-26 20:19:02 +0200

    💡Renamed component

--- components/mediaDetails/index.js
+++ components/mediaDetails/index.js
@@ -1,4 +1,4 @@
-export default function MovieDetails(props) {
+export default function MediaDetails(props) {
     return (
         <div className='absolute z-50 w-[95vw] h-[70vh] bg-slate-400 rounded-md flex flex-col overflow-x-hidden overflow-y-auto outline-none focus:outline-none'>
             <h1 className='text-4xl'>{props.id}</h1>

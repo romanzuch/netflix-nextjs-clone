5774ff214b74a381fc76b521e28559a305ef55fc
Roman <roman.zuchowski@gmail.com>
2022-04-26 20:07:30 +0200

    💡Initialising router and passing to child elements

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
-import '../styles/globals.css'
-import Header from '../components/header'
+import '../styles/globals.css';
+import Header from '../components/header';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <>
       <Header />
-      <Component {...pageProps} className='relative'/>
+      <Component {...pageProps} router={router} className='relative'/>
     </>
   )
 }

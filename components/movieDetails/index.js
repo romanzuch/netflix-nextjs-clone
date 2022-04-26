25b74f467b39b3572cffbfac9e3ec5825efe6adf
Roman <roman.zuchowski@gmail.com>
2022-04-26 20:05:32 +0200

    ➕Initial commit movieDetails

---
+++ components/movieDetails/index.js
@@ -0,0 +1,8 @@
+export default function MovieDetails(props) {
+    return (
+        <div className='absolute z-50 w-[95vw] h-[70vh] bg-slate-400 rounded-md flex flex-col overflow-x-hidden overflow-y-auto outline-none focus:outline-none'>
+            <h1 className='text-4xl'>{props.id}</h1>
+            <button onClick={() => {props.router.back()}}>Close</button>
+        </div>
+    )
+}

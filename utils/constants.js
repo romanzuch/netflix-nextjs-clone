b951a6e80ad2cef3fed373f0b8fc36605730322f
Roman <roman.zuchowski@gmail.com>
2022-04-26 21:50:15 +0200

    💡Refactored to remove warnings

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,9 @@
-export default {
+const constants = {
     BASE_URL: 'https://api.themoviedb.org/3',
     IMAGE_URL: 'https://image.tmdb.org/t/p/original/',
+    MOVIE_DETAILS_URL: 'https://api.themoviedb.org/3/movie',
+    TV_DETAILS_URL: 'https://api.themoviedb.org/3/tv',
     API_KEY: process.env.API_KEY,
 }
+
+export default constants;

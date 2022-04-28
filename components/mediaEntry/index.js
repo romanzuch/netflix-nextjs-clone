734643305e347e983fa275261fa3db49d3aa1184
Roman <roman.zuchowski@gmail.com>
2022-04-28 21:49:19 +0200

    💡Changed folder structure for mediaEntry

---
+++ components/mediaEntry/index.js
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import InfoIcon from '@mui/icons-material/Info';
+
+import { handleEntryClick } from '../../utils/mediaEntryHandler';
+
+function MediaEntry(props) {
+    
+    return (
+        <div
+            className='flex flex-col min-w-[192px] min-h-[108px] w-[10vw] h-[10vh] mr-4 transition ease-in-out delay-100 duration-200 transform hover:scale-110 cursor-pointer relative'
+            onClick={() => handleEntryClick(props.router, props.id)}
+        >
+            <Image
+                layout='responsive'
+                objectFit='cover'
+                objectPosition='center'
+                className='rounded-md'
+                width={192}
+                height={108}
+                src={props.image}
+                alt={props.title}
+            />
+        </div>
+    )
+}
+
+export default MediaEntry;
